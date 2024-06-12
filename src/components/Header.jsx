f9fe2b5e08c91c3b74c2525d24e42acf1edcf2c4
seunghun_lee <tmdgns5945@oasis.inje.ac.kr>
2024-06-13 01:58:09 +0900

    feat: fix slideClose Animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { keyframes } from "styled-components";
+import React, { useState, useEffect } from "react";
+import styled, { keyframes } from "styled-components";
 import { IoMdHome } from "react-icons/io";
 import { TiThMenu } from "react-icons/ti";
 import { IoCloseSharp } from "react-icons/io5";
@@ -14,7 +13,7 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    overflow-X: hidden;
+    overflow-x: hidden;
   }
 
   @media (max-width: 768px) {
@@ -29,164 +28,178 @@ const Container = styled.div`
     color: black;
     border-bottom: 3px solid lightgray;
 
-      svg {
-        font-size: 30px;
-      }
-
-      a {
-        text-decoration: none;
-        color: black;
-      }
+    svg {
+      font-size: 30px;
     }
-  `;
 
-  const HeaderFrame = styled.div`
-    @media (max-width: 768px) {
-      width: 90%;
-      height: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      z-index: 2;
-      background-color: white;
+    a {
+      text-decoration: none;
       color: black;
+    }
+  }
+`;
 
-        svg {
-          font-size: 30px;
-        }
-      }
-    `;
+const HeaderFrame = styled.div`
+  @media (max-width: 768px) {
+    width: 90%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    z-index: 2;
+    background-color: white;
+    color: black;
 
-    const TitleFrame = styled.div`
-      @media (max-width: 768px) {
-        width: 50%;
-        height: 100%;
-        display: flex;
-        align-items: center;
-      }
-    `;
+    svg {
+      font-size: 30px;
+    }
+  }
+`;
 
-    const slideOpen = keyframes`
-      from {
-        transform: translateX(200%);
-      }
-      to {
-        transform: translateX(0%);
-      }
-    `;
-    
-    const slideClose = keyframes`
-      from {
-        transform: translateX(200%);
-      }
-      to {
-        transform: translateX(0%);
-      }
-    `;
-  
+const TitleFrame = styled.div`
+  @media (max-width: 768px) {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+`;
 
-      const MenuContainer = styled.div`
-        @media (max-width: 768px) {
-          width: 60%;
-          height: 82dvh;
-          display: flex;
-          flex-direction: column;
-          right: 0px;
-          background-color: black;
-          opacity: 0.6;
-          position: absolute;
-          z-index: 1;
-          animation: ${({ openMenu }) => (openMenu ? slideOpen : slideClose)} 0.8s ease forwards; // openMenu 상태에 따라 애니메이션 적용
-        }
-      `;
+const slideOpen = keyframes`
+  from {
+    transform: translateX(200%);
+  }
+  to {
+    transform: translateX(0%);
+  }
+`;
 
-        const MenuFrame = styled.div`
-          @media (max-width: 768px) {
-            width: 100%;
-            height: 70%;
-          }
-        `;
+const slideClose = keyframes`
+  from {
+    transform: translateX(0%);
+  }
+  to {
+    transform: translateX(200%);
+  }
+`;
 
-          const MenuItem = styled.div`
-            width: 100%;
-            height: 8dvh;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            border-bottom: 1px solid white;
-            color: white;
-            &:hover {
-              background-color: white;
-              color: black;
-              font-size: 20px;
-            }
-          `;
-          
-        const MenuFootFrame = styled.div`
-          width: 100%;
-          height: 30%;
-          display: flex;
-          alignt-items: center;
-          justify-content: center;
-          img {
-            width: 80%;
-            height: 80%;
-          }
-        `;
+const MenuContainer = styled.div`
+  @media (max-width: 768px) {
+    width: 60%;
+    height: 82dvh;
+    display: flex;
+    flex-direction: column;
+    right: 0px;
+    background-color: black;
+    opacity: 0.6;
+    position: absolute;
+    z-index: 1;
+    animation: ${({ openMenu }) => (openMenu ? slideOpen : slideClose)} 0.8s ease forwards;
+  }
+`;
 
-      const Menu = () => {
-        return (
-          <MenuContainer>
-            <MenuFrame>
-              <MenuItem>
-                <h3>어플 사용 가이드</h3>
-              </MenuItem>
-              <MenuItem>
-                <h3>근처 맛집 찾기</h3>
-              </MenuItem>
-              <MenuItem>
-                <h3>근처 관광지 찾기</h3>
-              </MenuItem>
-              <MenuItem>
-                <h3>개발자 소개</h3>
-              </MenuItem>
-            </MenuFrame>
-            <MenuFootFrame>
-              <img src="./images/Icon.png"/>
-            </MenuFootFrame>
-          </MenuContainer>
-        )
-      }
+const MenuFrame = styled.div`
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 70%;
+  }
+`;
+
+const MenuItem = styled.div`
+  width: 100%;
+  height: 8dvh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid white;
+  color: white;
+  &:hover {
+    background-color: white;
+    color: black;
+    font-size: 20px;
+  }
+`;
+
+const MenuFootFrame = styled.div`
+  width: 100%;
+  height: 30%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 80%;
+    height: 80%;
+  }
+`;
+
+const Menu = ({ openMenu, onAnimationEnd }) => {
+  return (
+    <MenuContainer openMenu={openMenu} onAnimationEnd={onAnimationEnd}>
+      <MenuFrame>
+        <MenuItem>
+          <h3>어플 사용 가이드</h3>
+        </MenuItem>
+        <MenuItem>
+          <h3>근처 맛집 찾기</h3>
+        </MenuItem>
+        <MenuItem>
+          <h3>근처 관광지 찾기</h3>
+        </MenuItem>
+        <MenuItem>
+          <h3>개발자 소개</h3>
+        </MenuItem>
+      </MenuFrame>
+      <MenuFootFrame>
+        <img src="./images/Icon.png"/>
+      </MenuFootFrame>
+    </MenuContainer>
+  );
+};
 
 const Header = () => {
-
   const [openMenu, setMenu] = useState(false);
+  const [renderMenu, setRenderMenu] = useState(false);
+
+  useEffect(() => {
+    if (openMenu) {
+      setRenderMenu(true);
+    }
+  }, [openMenu]);
 
   const handleMenu = () => {
-    setMenu(!openMenu); 
-    console.log(openMenu);
+    if (openMenu) {
+      setMenu(false);
+    } else {
+      setMenu(true);
+    }
   };
-  
+
+  const handleAnimationEnd = () => {
+    if (!openMenu) {
+      setRenderMenu(false);
+    }
+  };
+
   return (
     <>
       <Container>
         <HeaderFrame>
           <Link to="/"><IoMdHome /></Link>
-            <TitleFrame>
+          <TitleFrame>
             <Link to="/" style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
               <PiCompassRoseLight />
               <h2>ANYWHERE</h2>
-              </Link>
-            </TitleFrame>
+            </Link>
+          </TitleFrame>
 
-            {openMenu ? (
-            <IoCloseSharp onClick={handleMenu}  style={{fontSize:"30px"}} />
-            ) : (
+          {openMenu ? (
+            <IoCloseSharp onClick={handleMenu} style={{fontSize:"30px"}} />
+          ) : (
             <TiThMenu onClick={handleMenu} />
-            )}
+          )}
         </HeaderFrame>
       </Container>
-      {openMenu && <Menu/>}
+      {renderMenu && <Menu openMenu={openMenu} onAnimationEnd={handleAnimationEnd} />}
     </>
   );
 };
